b9c7c970e9fa78072c05a76c4c014d3663459b95
秀策 <caijinzhu@dong10.com>
2018-11-23 09:06:48 +0800

    refactor(使用objc_copyImageNames 运行时获取系统的加载的动态/静态库信息):

--- XCRouterDemo/RouterDemo/RouterDemo/ViewController.m
+++ XCRouterDemo/RouterDemo/RouterDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
 
 @interface ViewController ()
 
@@ -16,7 +17,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    unsigned int imageCount = 0;
+    const char **images = objc_copyImageNames(&imageCount);
+    
+    for (int i = 0; i < imageCount; i++) {
+        NSLog(@"%s",images[i]);
+    }
+
+    UIResponder
 }
 
 
