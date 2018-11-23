ac5e405672069dc277bd7571637edbd70a620f8d
秀策 <caijinzhu@dong10.com>
2018-11-23 09:20:50 +0800

    refactor(添加Build 执行脚本):

--- XCRouterDemo/RouterDemo/RouterDemo/ViewController.m
+++ XCRouterDemo/RouterDemo/RouterDemo/ViewController.m
@@ -25,7 +25,6 @@
         NSLog(@"%s",images[i]);
     }
 
-    UIResponder
 }
 
 
---
+++ XCRouterDemo/RouterDemo/RouterDemo/XCFakeCode.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  XCFakeCode.sh
+#  RouterDemo
+#
+#  Created by Alexcai on 2018/11/23.
+#  Copyright © 2018 dongjiu. All rights reserved.
+echo "hello Fake Code"
+
