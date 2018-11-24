317373e9990f2777eeaf0488e16cf0c9a71e43ff
秀策 <caijinzhu@dong10.com>
2018-11-24 13:42:44 +0800

    refactor(XCRouterDemo): 添加PKPaymentAuthorizitionViewController

--- XCRouterDemo/RouterDemo/RouterDemo/ViewController.m
+++ XCRouterDemo/RouterDemo/RouterDemo/ViewController.m
@@ -10,7 +10,7 @@
 #import <objc/runtime.h>
 #import <PassKit/PassKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<PKPaymentAuthorizationViewControllerDelegate>
 
 @end
 
@@ -19,17 +19,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+}
+
+
+
+- (IBAction)clickPayButton:(UIButton *)sender {
     
+    NSDecimalNumber *payNumber = [[NSDecimalNumber alloc]initWithString:@"20.5"];
+    
+    PKPaymentSummaryItem *payItem = [PKPaymentSummaryItem summaryItemWithLabel:@"H" amount:payNumber];
+    
+    PKPaymentRequest *payRequest = [[PKPaymentRequest alloc]init];
+    payRequest.countryCode = @"CN";
+    payRequest.currencyCode = @"CNY";
+    payRequest.merchantIdentifier = @"com.day.pay";
+    payRequest.supportedNetworks = @[PKPaymentNetworkDiscover];
+    payRequest.merchantCapabilities = PKMerchantCapabilityCredit;
+    payRequest.paymentSummaryItems = @[payItem];
+    PKPaymentAuthorizationViewController * controller = [[PKPaymentAuthorizationViewController alloc]initWithPaymentRequest:payRequest];
+    controller.delegate = self;
+    [self presentViewController:controller animated:YES completion:nil];
+    
+}
+#pragma mark - private method
+
+- (void)p_testRuntimeForFramework{
     unsigned int imageCount = 0;
     const char **images = objc_copyImageNames(&imageCount);
     
     for (int i = 0; i < imageCount; i++) {
         NSLog(@"%s",images[i]);
     }
-
 }
 
-- (IBAction)clickPayButton:(UIButton *)sender {
-}
 
+#pragma mark
+- (void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller{
+    [controller dismissViewControllerAnimated:YES completion:nil];
+}
 @end
