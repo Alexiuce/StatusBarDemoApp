9e98e7c0dc210368a438f72c88852b555647d84e
alexiuce <alexiuce@qq.com>
2018-11-26 08:29:08 +0800

    refactor(添加XCAppEnterViewController):

---
+++ XCRouterDemo/RouterDemo/RouterDemo/Router/XCAppEnterViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCAppEnterViewController.h
+//  RouterDemo
+//
+//  Created by Alexcai on 2018/11/26.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCAppEnterViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCRouterDemo/RouterDemo/RouterDemo/Router/XCAppEnterViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCAppEnterViewController.m
+//  RouterDemo
+//
+//  Created by Alexcai on 2018/11/26.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCAppEnterViewController.h"
+
+@interface XCAppEnterViewController ()
+
+@end
+
+@implementation XCAppEnterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
