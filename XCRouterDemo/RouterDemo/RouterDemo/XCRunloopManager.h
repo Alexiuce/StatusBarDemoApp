147e93bf658abbfddd55d3c642543bf5155a5a5a
alexiuce <alexiuce@qq.com>
2018-11-29 08:10:06 +0800

    refactor(添加Runloop管理单利):

---
+++ XCRouterDemo/RouterDemo/RouterDemo/XCRunloopManager.h
@@ -0,0 +1,20 @@
+//
+//  XCRunloopManager.h
+//  RouterDemo
+//
+//  Created by Alexcai on 2018/11/29.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCRunloopManager : NSObject
+
++ (instancetype)shareManager;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCRouterDemo/RouterDemo/RouterDemo/XCRunloopManager.m
@@ -0,0 +1,68 @@
+//
+//  XCRunloopManager.m
+//  RouterDemo
+//
+//  Created by Alexcai on 2018/11/29.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCRunloopManager.h"
+
+@implementation XCRunloopManager
+
++ (instancetype)shareManager{
+    static dispatch_once_t onceToken;
+    XCRunloopManager *__instance = nil;
+    dispatch_once(&onceToken, ^{
+        __instance = [self new];
+    });
+    return __instance;
+}
+
+
+- (void)addRunloopObserver{
+    /**
+     kCFRunLoopEntry   ==   1 进入runloop ,
+     kCFRunLoopBeforeTimers == 2   即将处理Timer,
+     kCFRunLoopBeforeSources == 4 即将处理source,
+     kCFRunLoopBeforeWaiting  == 32 即将开始休眠,
+     kCFRunLoopAfterWaiting == 64 从休眠唤醒,
+     kCFRunLoopExit = 128   退出runloop,
+     kCFRunLoopAllActivities  所有状态;
+     */
+    
+    //  创建observer
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(CFAllocatorGetDefault(), kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+        
+        switch (activity) {
+            case kCFRunLoopEntry:{
+                NSLog(@"----进入runloop---");
+                break;
+            }
+            case kCFRunLoopBeforeTimers:{
+                NSLog(@"---- runloop准备处理Timer ---");
+                break;
+            }
+            case kCFRunLoopBeforeSources:{
+                NSLog(@"---- runloop准备处理Source ---");
+                break;
+            }
+            case kCFRunLoopBeforeWaiting:{
+                NSLog(@"---- runloop即将进入休眠 ---");
+                break;
+            }
+            case kCFRunLoopAfterWaiting:{
+                NSLog(@"---- runloop从休眠中唤醒 ---");
+                break;
+            }
+            default:
+                break;
+        }
+        
+    });
+    
+    // 添加观察者：监听RunLoop的状态
+    CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
+}
+
+@end
