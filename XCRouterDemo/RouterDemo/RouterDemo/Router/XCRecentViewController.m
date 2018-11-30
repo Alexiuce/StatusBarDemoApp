39dbe6c7bd6016b53aabbbe350cee122d689f57b
alexiuce <alexiuce@qq.com>
2018-11-30 08:50:06 +0800

    refactor(XCRecentViewController): 添加GCD 代码测试

--- XCRouterDemo/RouterDemo/RouterDemo/Router/XCRecentViewController.m
+++ XCRouterDemo/RouterDemo/RouterDemo/Router/XCRecentViewController.m
@@ -28,30 +28,50 @@
 /** set_target_queue */
 - (void)p_testForGCD{
     
-    dispatch_queue_t q1 = dispatch_queue_create("com.alexiuc.github",NULL);
+//    DISPATCH_QUEUE_CONCURRENT
+    // DISPATCH_QUEUE_SERIAL == NULL
+    
+    dispatch_queue_t q1 = dispatch_queue_create("com.alexiuc.github",DISPATCH_QUEUE_CONCURRENT);
     dispatch_async(q1, ^{
         NSLog(@"q1  doing....%@",NSThread.currentThread);
     });
-    
-    dispatch_queue_t q3 = dispatch_queue_create("com.aaa.github", NULL);
-    dispatch_async(q3, ^{
-        NSLog(@"q3 diong.....%@",NSThread.currentThread);
+    dispatch_async(q1, ^{
+            NSLog(@"t1 .....");
+    });
+    dispatch_async(q1, ^{
+        NSLog(@"t2.....");
+    });
+    dispatch_async(q1, ^{
+        NSLog(@"t3.....");
     });
     
-    dispatch_queue_t q4 = dispatch_queue_create("com.aaa.github", NULL);
-    dispatch_async(q4, ^{
-        NSLog(@"q4 diong.....%@",NSThread.currentThread);
-    });
-    
-    dispatch_queue_t q5 = dispatch_queue_create("com.aaa.github", NULL);
-    dispatch_async(q5, ^{
-        NSLog(@"q5 diong.....%@",NSThread.currentThread);
-    });
-    
-    dispatch_queue_t q6 = dispatch_queue_create("com.aaa.github", NULL);
-    dispatch_async(q6, ^{
-        NSLog(@"q6 diong.....%@",NSThread.currentThread);
-    });
+//    dispatch_queue_t q3 = dispatch_queue_create("com.aaa.github", NULL);
+//    dispatch_async(q3, ^{
+//        NSLog(@"q3 diong.....%@",NSThread.currentThread);
+//    });
+//
+//    dispatch_async(q3, ^{
+//        NSLog(@" q3 ...2.....");
+//    });
+//    dispatch_async(q3, ^{
+//        NSLog(@"q3... 3.....");
+//    });
+//
+//
+//    dispatch_queue_t q4 = dispatch_queue_create("com.aaa.github", NULL);
+//    dispatch_async(q4, ^{
+//        NSLog(@"q4 diong.....%@",NSThread.currentThread);
+//    });
+//
+//    dispatch_queue_t q5 = dispatch_queue_create("com.aaa.github", NULL);
+//    dispatch_async(q5, ^{
+//        NSLog(@"q5 diong.....%@",NSThread.currentThread);
+//    });
+//
+//    dispatch_queue_t q6 = dispatch_queue_create("com.aaa.github", NULL);
+//    dispatch_async(q6, ^{
+//        NSLog(@"q6 diong.....%@",NSThread.currentThread);
+//    });
     
     
     
