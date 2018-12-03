e46a28ea4d95ac053f5a26224bad6f09c852992c
alexiuce <alexiuce@qq.com>
2018-12-03 08:24:56 +0800

    refactor(XCScrollView): 添加类文件

---
+++ XCRouterDemo/RouterDemo/RouterDemo/Views/XCScrollView.h
@@ -0,0 +1,17 @@
+//
+//  XCScrollView.h
+//  RouterDemo
+//
+//  Created by Alexcai on 2018/12/3.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCScrollView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCRouterDemo/RouterDemo/RouterDemo/Views/XCScrollView.m
@@ -0,0 +1,15 @@
+//
+//  XCScrollView.m
+//  RouterDemo
+//
+//  Created by Alexcai on 2018/12/3.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCScrollView.h"
+
+@implementation XCScrollView
+
+
+
+@end
