e2d0d1662ad0f88c53a61571386bdcaffbf94b5a
alexiuce <alexiuce@qq.com>
2018-12-03 08:58:40 +0800

    refactor(XCScrollView): 添加拖拽手势,模式实现Scrollview原理

--- XCRouterDemo/RouterDemo/RouterDemo/Views/XCScrollView.m
+++ XCRouterDemo/RouterDemo/RouterDemo/Views/XCScrollView.m
@@ -8,8 +8,35 @@
 
 #import "XCScrollView.h"
 
+@interface XCScrollView ()
+
+@property (nonatomic, assign) CGPoint lastPoint;
+
+@end
+
+
 @implementation XCScrollView
 
+- (void)awakeFromNib{
+    [super awakeFromNib];
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(p_panInView:)];
+    [self addGestureRecognizer:pan];
+    
+}
 
 
+
+#pragma mark - Pan Gesture handle
+- (void)p_panInView:(UIPanGestureRecognizer *)pan{
+    CGPoint p = [pan translationInView:self];
+    self.lastPoint = CGPointMake(p.x + self.lastPoint.x, p.y + self.lastPoint.y);
+    NSLog(@"%f",p.x);
+    CGRect b = self.bounds;
+    b.origin.x  = - self.lastPoint.x;
+    b.origin.y = - self.lastPoint.y;
+    self.bounds = b;
+    [pan setTranslation:CGPointZero inView:self];
+   
+}
 @end
