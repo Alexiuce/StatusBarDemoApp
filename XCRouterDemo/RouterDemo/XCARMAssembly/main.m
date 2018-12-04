4a746eac6dbcb63ac6ec2c2059c3376e006a855c
alexiuce <alexiuce@qq.com>
2018-12-04 08:38:02 +0800

    feat(Target #XCARMAssembly): 添加汇编Target

---
+++ XCRouterDemo/RouterDemo/XCARMAssembly/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  XCARMAssembly
+//
+//  Created by Alexcai on 2018/12/3.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        
+        NSLog(@"a + b = %d",c);
+        
+        
+//        mov al, 2;
+//        add al,al;
+//        add al,al;
+//        add al,al;
+    }
+    return 0;
+}
