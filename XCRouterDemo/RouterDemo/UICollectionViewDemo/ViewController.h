6bb80b5d170541f828494ee902785c44bcc3d43e
alexiuce <alexiuce@qq.com>
2018-12-06 09:27:01 +0800

    refactor(UICollectionViewDemo): Prefetching APIs

---
+++ XCRouterDemo/RouterDemo/UICollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewDemo
+//
+//  Created by Alexcai on 2018/12/6.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
