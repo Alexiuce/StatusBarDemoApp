3f8f7787ff4551bc9170ebef812f13f47b41db5c
alexiuce <alexiuce@qq.com>
2018-12-11 13:16:13 +0800

    refactor(CADemo):
    添加views/controllers/viewmodes

---
+++ XCRouterDemo/RouterDemo/CADemo/Views/XCDrawingView.m
@@ -0,0 +1,21 @@
+//
+//  XCDrawingView.m
+//  CADemo
+//
+//  Created by Alexcai on 2018/12/11.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCDrawingView.h"
+
+@implementation XCDrawingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
