601724745621cec6d10998b88a07ee8929f82222
alexiuce <alexiuce@qq.com>
2018-12-13 08:37:23 +0800

    refactor(CADemo): 添加CollectionViewController 测试ImageIOwent

---
+++ XCRouterDemo/RouterDemo/CADemo/Controllers/XCCollectionViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCCollectionViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2018/12/13.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCCollectionViewController.h"
+
+@interface XCCollectionViewController ()
+
+@end
+
+@implementation XCCollectionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
