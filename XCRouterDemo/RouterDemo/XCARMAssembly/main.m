79aec63346f241487ec031ed275daab6709207bc
alexiuce <alexiuce@qq.com>
2018-12-16 15:39:42 +0800

    refactor(XCARMAssembly): printf("0x%.14zX"):十六进制方式输出指定长度的内容

--- XCRouterDemo/RouterDemo/XCARMAssembly/main.m
+++ XCRouterDemo/RouterDemo/XCARMAssembly/main.m
@@ -41,12 +41,17 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"a + b = %d",a1);
         
-        
+        /** 声明一个无符号8bit长度的int类型 */
+        uint8 u8 = 12;
+        /** 定义一个可以存放指针的变量*/
+        uintptr_t p8 =(uintptr_t) &u8;
         /** 至少需要8bit的无符号整型 */
         uint_least8_t lest8 = 0;
         
         /** 快速计算的 int8 类型 */
         int_fast8_t fast8 = 0;
+        /** 以16进制方式输出 %x(小写) %X(大写) int值*/
+        printf("0x%.14zX",p8);
         
 //        mov al, 2;
 //        add al,al;
