bf43fee4b414c83a432a326ddc835d70b58ceb8c
alexiuce <alexiuce@qq.com>
2018-12-18 08:28:33 +0800

    refactor(XCAutoLayoutDemo:ViewController): p_missingViewDemo添加相互矛盾的视图约束会导致视图无法正常显示出来

--- XCRouterDemo/RouterDemo/XCAutoLayoutDemo/ViewController.m
+++ XCRouterDemo/RouterDemo/XCAutoLayoutDemo/ViewController.m
@@ -32,6 +32,28 @@
     /** 设置宽度 */
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[view]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(view)]];
     
+    
+    
+    UIView *viewA = [[UIView alloc]initWithFrame:CGRectMake(10, 10, 20, 20)];
+    viewA.backgroundColor = UIColor.blueColor;
+    viewA.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:viewA];
+    
+    UIView *viewB = [[UIView alloc]initWithFrame:CGRectMake(40, 40, 20, 20)];
+    viewB.backgroundColor = UIColor.orangeColor;
+    viewB.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:viewB];
+    
+    /** 添加约束 : 设置相互矛盾的约束会导致视图可能 missing 如下例: */
+    NSLayoutConstraint *constraint ;
+    /** 设置viewA的宽度约束为viewB宽度的2倍*/
+    constraint = [NSLayoutConstraint constraintWithItem:viewA attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:viewB attribute:NSLayoutAttributeWidth multiplier:2.0f constant:0.0f];
+    [self.view addConstraint:constraint];
+    /** 设置viewB的宽度为viewA宽度的3倍 */
+    constraint = [NSLayoutConstraint constraintWithItem:viewB attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:viewA attribute:NSLayoutAttributeWidth multiplier:3.0f constant:0];
+    [self.view addConstraint:constraint];
+    
+    
 }
 
 @end
