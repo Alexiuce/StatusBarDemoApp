0221e86ce95f10346cd205482b942da16ba93de2
alexiuce <alexiuce@qq.com>
2018-12-25 15:43:09 +0800

    refactor(XCTestApp): 添加登录测试代码

--- XCRouterDemo/RouterDemo/XCTestApp/ViewController.m
+++ XCRouterDemo/RouterDemo/XCTestApp/ViewController.m
@@ -8,16 +8,26 @@
 
 #import "ViewController.h"
 
+@interface ViewController ()
+
+@property (weak) IBOutlet NSTextField *userName;
+
+@property (weak) IBOutlet NSSecureTextField *password;
+
+@property (weak) IBOutlet NSTextField *infoLabel;
+
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     /** 获取所有正在运行的app 信息 */
-    NSArray *runingApp = NSWorkspace.sharedWorkspace.runningApplications;
-    for (NSRunningApplication *app in runingApp) {
-        NSLog(@"%@",app.localizedName);
-    }
+//    NSArray *runingApp = NSWorkspace.sharedWorkspace.runningApplications;
+//    for (NSRunningApplication *app in runingApp) {
+//        NSLog(@"%@",app.localizedName);
+//    }
     
     // Do any additional setup after loading the view.
 }
@@ -27,6 +37,17 @@
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+- (IBAction)login:(NSButton *)sender {
+    
+    if ([self.userName.stringValue isEqualToString:@"alex"] && [self.password.stringValue isEqualToString:@"123"]) {
+        NSLog(@"登录成功");
+        self.infoLabel.stringValue = @"登录成功";
+    }else{
+        NSLog(@"登录失败");
+        self.infoLabel.stringValue = @"登录失败";
+    }
+    
 }
 
 
