326db8754465c70250b083b740080d463c8ee917
alexiuce <alexiuce@qq.com>
2018-12-31 15:21:43 +0800

    refactor(XCDrawing): 添加自定义view

---
+++ XCRouterDemo/RouterDemo/XCDrawing/Views/XCDrawingView.m
@@ -0,0 +1,13 @@
+//
+//  XCDrawingView.m
+//  XCDrawing
+//
+//  Created by Alexcai on 2018/12/31.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+#import "XCDrawingView.h"
+
+@implementation XCDrawingView
+
+@end
