f37f40d7e067d8d09b9d40a6101f7422879cdff6
alexiuce <alexiuce@qq.com>
2019-01-01 14:48:29 +0800

    refactor(XCDrawing): XCDrawView 添加drawRect 方法,

--- XCRouterDemo/RouterDemo/XCDrawing/Views/XCDrawingView.m
+++ XCRouterDemo/RouterDemo/XCDrawing/Views/XCDrawingView.m
@@ -10,4 +10,14 @@
 
 @implementation XCDrawingView
 
+
+- (void)drawRect:(CGRect)rect{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    /** UIKit 持有的CGContext 以左上角为0,0 ; 否则以左下角为0,0*/
+    CGContextMoveToPoint(ctx, 10, 10);
+    CGContextAddLineToPoint(ctx, 50, 50);
+    
+    CGContextStrokePath(ctx);
+}
+
 @end
