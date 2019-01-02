b03f5dc94b740101da7e4dbbfa36ecb14b432a7f
alexiuce <alexiuce@qq.com>
2019-01-02 11:07:55 +0800

    refactor(CADemo): XCCubeController: 添加GLK 光照代码 测试

--- XCRouterDemo/RouterDemo/CADemo/Controllers/XCCubeController.m
+++ XCRouterDemo/RouterDemo/CADemo/Controllers/XCCubeController.m
@@ -7,6 +7,12 @@
 //
 
 #import "XCCubeController.h"
+#import <GLKit/GLKit.h>
+
+
+#define LIGHT_DIRECTION 0, 1, -0.5
+
+static CGFloat const AMBIENT_LIGHT = 0.5;
 
 @interface XCCubeController ()
 @property (weak, nonatomic) IBOutlet UIView *containerView;
@@ -70,5 +76,54 @@
     CGSize containerSize = self.containerView.bounds.size;
     face.center = CGPointMake(containerSize.width * 0.5, containerSize.height * 0.5);
     face.layer.transform = trans;
+    
+    /** 添加光照效果 */
+    [self addLightingToFace:face.layer];
+}
+
+
+- (void)addLightingToFace:(CALayer *)face{
+    CALayer *layer = [CALayer layer];
+    layer.frame = face.bounds;
+    [face addSublayer:layer];
+    
+    //(GLKMatrix4 has the same structure as CATransform3D)
+
+    CATransform3D transform = face.transform;
+
+    GLKMatrix4 matrix4 = *(GLKMatrix4 *)&transform;
+
+    GLKMatrix3 matrix3 = GLKMatrix4GetMatrix3(matrix4);
+
+
+    //get face normal
+    GLKVector3 normal = GLKVector3Make(0, 0, 1);
+    normal = GLKMatrix3MultiplyVector3(matrix3, normal);
+    normal = GLKVector3Normalize(normal);
+
+    //get dot product with light direction
+    GLKVector3 light = GLKVector3Normalize(GLKVector3Make(LIGHT_DIRECTION));
+    float dotProduct = GLKVector3DotProduct(light, normal);
+
+    
+    
+//    /** GLK (GLKMatrix4 has the same structure as CATransform3D) */
+//    CATransform3D transform = face.transform;
+//    GLKMatrix4 matrix4 = *(GLKMatrix4*)&transform;
+//    GLKMatrix3 matrix3 = GLKMatrix4GetMatrix3(matrix4);
+//    /** 计算正常lighting */
+//    GLKVector3 normal = GLKVector3Make(0, 0, 1);
+//    normal = GLKMatrix3MultiplyVector3(matrix3, normal);
+//    normal = GLKVector3Normalize(normal);
+//
+//    /** 光源点 */
+//    GLKVector3 light = GLKVector3Normalize(GLKVector3Make(LIGHT_DIRECTION));
+//    float dotProduct = GLKVector3DotProduct(light, normal);
+    /** 设置亮度 */
+
+    CGFloat shadow = 1 + dotProduct - AMBIENT_LIGHT;
+    UIColor *lightColor = [UIColor colorWithWhite:0 alpha:shadow];
+    layer.backgroundColor = lightColor.CGColor;
+    
 }
 @end
