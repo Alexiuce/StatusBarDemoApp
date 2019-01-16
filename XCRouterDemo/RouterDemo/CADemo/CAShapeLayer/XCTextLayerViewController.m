062203d98af6cf8a4f835a988524fc3d2740c608
alexiuce <alexiuce@qq.com>
2019-01-16 10:39:19 +0800

    refactor(#CADemo): attributedTextDemo : 设置带属性的文本

--- XCRouterDemo/RouterDemo/CADemo/CAShapeLayer/XCTextLayerViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/CAShapeLayer/XCTextLayerViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = UIColor.orangeColor;
-    [self textDemo];
+    [self attributedTextDemo];
 }
 
 
@@ -49,5 +49,27 @@
     
 }
 
+- (void)attributedTextDemo{
+    CATextLayer *textLayer = [CATextLayer layer];
+    textLayer.frame = CGRectInset(self.view.bounds, 120, 330);
+    [self.view.layer addSublayer:textLayer];
+    NSString *text = @"Lorem ipsum dolor sit amet, consectetur adipiscing \
+        elit. Quisque massa arcu, eleifend vel varius in, facilisis pulvinar \
+        leo. Nunc quis nunc at mauris pharetra condimentum ut ac neque. Nunc \
+        elementum, libero ut porttitor dictum, diam odio congue lacus, vel \
+        fringilla sapien diam at purus. Etiam suscipit pretium nunc sit amet \
+        lobortis;";
+    NSMutableAttributedString *m_string = [[NSMutableAttributedString alloc]initWithString:text];
+    UIFont *font = [UIFont fontWithName:@"American Typewriter" size:20];
+    /** 设置字体 与 下划线效果 */
+    NSDictionary *attribDict = @{NSFontAttributeName:font,
+                                 NSUnderlineStyleAttributeName :@(NSUnderlineStyleThick)
+                                 
+                                 };
+    
+    [m_string setAttributes:attribDict range:NSMakeRange(4, 4)];
+    textLayer.wrapped = YES;
+    textLayer.string = m_string;
+}
 
 @end
