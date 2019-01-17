8f3827f045738b470242d2a198a7e45407925e51
alexiuce <alexiuce@qq.com>
2019-01-17 08:32:39 +0800

    refactor(#CADemo): XCTransformLayerViewController: 添加CATransfromLayer 测试

--- XCRouterDemo/RouterDemo/CADemo/CAShapeLayer/XCTransformLayerViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/CAShapeLayer/XCTransformLayerViewController.m
@@ -19,8 +19,30 @@
     // Do any additional setup after loading the view.
     self.title = NSStringFromClass(self.class);
     self.view.backgroundColor = UIColor.lightGrayColor;
+    [self transfromLayerDemo];
 }
 
+- (void)transfromLayerDemo{
+    UIView *containerView = [[UIView alloc]initWithFrame:CGRectInset(self.view.bounds, 20, 100)];
+    [self.view addSubview:containerView];
+    [containerView.layer addSublayer:[self calayerWithTransform:CATransform3DIdentity]];
+    
+}
+- (CALayer *)calayerWithTransform:(CATransform3D)transform{
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(-50, -50,  100, 100);
+//    layer.transform = transform;
+    CGFloat red = arc4random_uniform(255);
+    CGFloat green = arc4random_uniform(255);
+    CGFloat blue = arc4random_uniform(255);
+    layer.backgroundColor = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:1.0].CGColor;
+    return layer;
+}
 
+- (CALayer *)cubeWithTransfrom:(CATransform3D)transform{
+    CATransformLayer *cube = [CATransformLayer layer];
+    
+    return cube;
+}
 
 @end
