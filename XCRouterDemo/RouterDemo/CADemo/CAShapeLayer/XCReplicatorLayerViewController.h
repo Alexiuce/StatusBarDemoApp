4bbe3354c46c4234d340ddd0587547d3d2538c2f
alexiuce <alexiuce@qq.com>
2019-01-17 13:48:13 +0800

    refactor(#CADemo): XCReplicatorLayerViewController: 添加ReplicatorLayer Demo

---
+++ XCRouterDemo/RouterDemo/CADemo/CAShapeLayer/XCReplicatorLayerViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCReplicatorLayerViewController.h
+//  CADemo
+//
+//  Created by Alexcai on 2019/1/17.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCReplicatorLayerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCRouterDemo/RouterDemo/CADemo/CAShapeLayer/XCReplicatorLayerViewController.m
@@ -0,0 +1,25 @@
+//
+//  XCReplicatorLayerViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/1/17.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCReplicatorLayerViewController.h"
+
+@interface XCReplicatorLayerViewController ()
+
+@end
+
+@implementation XCReplicatorLayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = NSStringFromClass(self.class);
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+
+
+@end
