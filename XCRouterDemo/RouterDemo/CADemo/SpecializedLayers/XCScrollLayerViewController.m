cf68280be8d218cd7cf5b46f02687f901aeb4a2a
alexiuce <alexiuce@qq.com>
2019-01-21 09:01:24 +0800

    refactor(#CADemo): ScrollLayer 移动SubLayer

--- XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCScrollLayerViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCScrollLayerViewController.m
@@ -12,6 +12,10 @@
 
 @property (nonatomic, weak) CAScrollLayer *scrollLayer;
 
+
+@property (nonatomic, assign) CGPoint beginPoint;
+@property (nonatomic, assign) CGPoint currentPoint;
+
 @end
 
 @implementation XCScrollLayerViewController
@@ -29,11 +33,36 @@
     sl.backgroundColor = UIColor.lightGrayColor.CGColor;
     sl.frame = CGRectMake(100, 150, 200, 200);
     [self.view.layer addSublayer:sl];
-    UIImage *img = [UIImage imageNamed:@"scrollImage.jpg"];
-    sl.contents = (__bridge id _Nullable)(img.CGImage);
+    
+    NSString *picturePath = [NSBundle.mainBundle pathForResource:@"Image/scrollImage" ofType:@"jpg"];
+    //    UIImage *img = [UIImage imageNamed:@"scrollImage.jpg"];
+    UIImage *img = [UIImage imageWithContentsOfFile:picturePath];
+    
+    CALayer *imageLayer = [CALayer layer];
+    imageLayer.frame = CGRectMake(0, 0, img.size.width, img.size.height);
+    imageLayer.contents = (__bridge id _Nullable)(img.CGImage);
+    [sl addSublayer:imageLayer];
     self.scrollLayer = sl;
+    /** 设置滚动位置*/
+    [sl scrollToPoint:CGPointMake(150, 150)];
 }
 
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    self.beginPoint = [[touches anyObject] locationInView:self.view];
+}
 
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    UITouch *t = [touches anyObject];
+    CGPoint location = [t locationInView:self.view];
+    
+    CGFloat deltaX = location.x + self.beginPoint.x;
+    CGFloat deltaY = location.y + self.beginPoint.y;
+    
+    CGPoint p = CGPointMake(self.currentPoint.x + deltaX, self.currentPoint.y + deltaY);
+    [self.scrollLayer scrollToPoint:p];
+    self.currentPoint = p;
+  
+    
+}
 @end
