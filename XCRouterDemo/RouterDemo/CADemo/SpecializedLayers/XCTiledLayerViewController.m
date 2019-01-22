de0995cec77f4ea8a45e57a0641d13ad841a2b2e
alexiuce <alexiuce@qq.com>
2019-01-22 09:10:41 +0800

    refactor(#CADemo): 添加XCTiledLayerController

---
+++ XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCTiledLayerViewController.m
@@ -0,0 +1,34 @@
+//
+//  XCTitledLayerViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/1/22.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCTiledLayerViewController.h"
+
+@interface XCTiledLayerViewController ()
+
+@end
+
+@implementation XCTiledLayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = NSStringFromClass(self.class);
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
