16f4c524eb22a002588463470a3878981a9bea5c
alexiuce <alexiuce@qq.com>
2019-01-22 14:51:50 +0800

    refactor(#CADemo): url
    中文编码/解码

--- XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCTiledLayerViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCTiledLayerViewController.m
@@ -32,6 +32,16 @@
     UIImage *img = [UIImage imageWithContentsOfFile:picturePath];
     CGFloat t = ceil(tiledSize);
     NSLog(@"%f",t);
+    NSString*hStr =@"你好啊";
+    NSString*hString = [hStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSLog(@"hString === %@",hString);
+    NSString*str3 =@"\u5982\u4f55\u8054\u7cfb\u5ba2\u670d\u4eba\u5458\uff1f";
+//    NSString*str5 = [str3 stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString*str5 = [str3 stringByRemovingPercentEncoding];
+    
+    NSLog(@"str5 ==== %@",str5);
+    
+   
     
 }
 
