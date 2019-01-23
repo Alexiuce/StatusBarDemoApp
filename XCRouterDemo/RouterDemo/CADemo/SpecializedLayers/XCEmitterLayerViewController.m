fa6c87ea5164f4e632cd485a8b81acb2445e6411
alexiuce <alexiuce@qq.com>
2019-01-23 08:50:55 +0800

    refactor(#CADemo): XCEmitterLayerController

---
+++ XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCEmitterLayerViewController.m
@@ -0,0 +1,34 @@
+//
+//  XCEmitterLayerViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/1/23.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCEmitterLayerViewController.h"
+
+@interface XCEmitterLayerViewController ()
+
+@end
+
+@implementation XCEmitterLayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = NSStringFromClass(self.class);
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
