9107aa577f32ba43aeeeb1459e03010e40865de4
alexiuce <alexiuce@qq.com>
2019-01-25 10:42:03 +0800

    refactor(#CADemo): EmitterLayer test

--- XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCEmitterLayerViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/SpecializedLayers/XCEmitterLayerViewController.m
@@ -19,8 +19,9 @@
     // Do any additional setup after loading the view.
     self.title = NSStringFromClass(self.class);
     self.view.backgroundColor = UIColor.whiteColor;
+    [self emitterDemo];
 }
-
+/** EmitterLayer test */
 - (void)emitterDemo{
     CAEmitterLayer *emitterLayer = [CAEmitterLayer layer];
     emitterLayer.frame = self.view.bounds;
@@ -28,6 +29,20 @@
     
     emitterLayer.renderMode = kCAEmitterLayerAdditive;
     emitterLayer.emitterPosition = CGPointMake(emitterLayer.frame.size.width * 0.5, emitterLayer.frame.size.height * 0.5);
+    
+    CAEmitterCell *cell = [CAEmitterCell emitterCell];
+    cell.contents = (__bridge id _Nullable)([UIImage imageNamed:@"icon-service4"].CGImage);
+    
+    cell.birthRate = 150;
+    cell.lifetime = 5.0;
+    cell.color = [UIColor colorWithRed:1 green:0.5 blue:0.1 alpha:1].CGColor;
+    cell.alphaSpeed = -0.4;
+    cell.velocity = 40;
+    cell.velocityRange = 50;
+    cell.emissionRange = M_PI ;
+    
+    emitterLayer.emitterCells = @[cell];
+    
     
     
 }
