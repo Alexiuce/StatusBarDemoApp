3c19b625e5d459acef08c8079163387e54d06f37
alexiuce <alexiuce@qq.com>
2019-01-28 09:08:06 +0800

    refactor(#CADmoe): colorLayer change animation : CATransaction

--- XCRouterDemo/RouterDemo/CADemo/Motions/XCImplictViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/Motions/XCImplictViewController.m
@@ -12,6 +12,8 @@
 
 @interface XCImplictViewController ()
 
+@property (nonatomic, weak) CALayer *colorLayer;
+
 @end
 
 @implementation XCImplictViewController
@@ -34,20 +36,26 @@
     [btn setBackgroundColor:UIColor.lightGrayColor];
     [btn addTarget:self action:@selector(clickButtonForChangeColor:) forControlEvents:UIControlEventTouchUpInside];
     
+    CALayer *layer = [CALayer layer];
+    layer.frame = XCFrame(15, 150, 100, 100);
+    layer.backgroundColor = UIColor.redColor.CGColor;
+    [self.view.layer addSublayer:layer];
+    self.colorLayer = layer;
+    
 }
 
 #pragma mark IBAction
 - (void)clickButtonForChangeColor:(UIButton *)btn{
+    CGFloat red = arc4random_uniform(255) / 255.0;
+    CGFloat green = arc4random_uniform(255) / 255.0;
+    CGFloat blue = arc4random_uniform(255) / 255.0;
     
     [CATransaction begin];
     [CATransaction setAnimationDuration:2.0];
     [CATransaction setCompletionBlock:^{
         NSLog(@"finished....");
     }];
-    CGFloat red = arc4random_uniform(255) / 255.0;
-    CGFloat green = arc4random_uniform(255) / 255.0;
-    CGFloat blue = arc4random_uniform(255) / 255.0;
-    btn.layer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1].CGColor;
+    self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1].CGColor;
     [CATransaction commit];
 }
 
