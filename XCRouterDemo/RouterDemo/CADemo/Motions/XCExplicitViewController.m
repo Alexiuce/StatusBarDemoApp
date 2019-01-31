aa53301997c3308551b0352a8e53d3ce7636e40e
alexiuce <alexiuce@qq.com>
2019-01-31 10:01:50 +0800

    refactor(#CADemo): 添加XCExplicit Animation demo controller

---
+++ XCRouterDemo/RouterDemo/CADemo/Motions/XCExplicitViewController.m
@@ -0,0 +1,24 @@
+//
+//  XCExplicitViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/1/31.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCExplicitViewController.h"
+
+@interface XCExplicitViewController ()
+
+@end
+
+@implementation XCExplicitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = NSStringFromClass(self.class);
+    self.view.backgroundColor = UIColor.whiteColor;}
+
+
+
+@end
