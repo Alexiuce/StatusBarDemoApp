c992fae0169b28443ef1d1cd12d1971bb194f6b8
alexiuce <alexiuce@qq.com>
2019-02-14 14:43:15 +0800

    refactor(#CADemo): XCTimeProtocolViewController: 添加CATimeProcotol DEMO

---
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeProtocolViewController.m
@@ -0,0 +1,26 @@
+//
+//  XCTimeProtocolViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/2/14.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCTimeProtocolViewController.h"
+
+@interface XCTimeProtocolViewController ()
+
+@end
+
+@implementation XCTimeProtocolViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = NSStringFromClass(self.class);
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+
+
+
+@end
