dc5c322ed5bb312faf5969c359b98264fbf1821e
alexiuce <alexiuce@qq.com>
2019-02-15 09:56:23 +0800

    refactor(#CADemo): XCTimeProtocolViewController: durationAndRepreatCountDemo

--- XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeProtocolViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeProtocolViewController.m
@@ -8,7 +8,12 @@
 
 #import "XCTimeProtocolViewController.h"
 
-@interface XCTimeProtocolViewController ()
+@interface XCTimeProtocolViewController ()<CAAnimationDelegate>
+
+@property (nonatomic, weak) UITextField *durationField;
+@property (nonatomic, weak) UITextField *repeatCountField;
+@property (nonatomic, weak) UIButton *startButton;
+@property (nonatomic, weak) CALayer *layer;
 
 @end
 
@@ -18,10 +23,77 @@
     [super viewDidLoad];
     self.title = NSStringFromClass(self.class);
     self.view.backgroundColor = UIColor.whiteColor;
-    [self qrCodeImageDemo];
+    [self durationAndRepreatCountDemo];
 }
 
+#pragma mark Duration & RepeatCount
 
+- (void)durationAndRepreatCountDemo{
+    UIView *lineView = [[UIView alloc]init];
+    lineView.backgroundColor = UIColor.lightGrayColor;
+    lineView.frame = CGRectMake(0,0 , self.view.bounds.size.width, 250);
+    [self.view addSubview:lineView];
+    
+    CALayer *redLayer = [CALayer layer];
+    redLayer.backgroundColor = UIColor.redColor.CGColor;
+    redLayer.frame = (CGRect){0,0,50,50};
+    redLayer.position = (CGPoint){150,120};
+    [self.view.layer addSublayer:redLayer];
+    self.layer = redLayer;
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = UIColor.blueColor;
+    btn.frame = CGRectMake(self.view.bounds.size.width - 80, 200, 70, 30);
+    [btn setTitle:@"Strat" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(clickStartButtonEvent:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    self.startButton = btn;
+    
+    UILabel *durationLabel = [[UILabel alloc]init];
+    durationLabel.text = @"Duration:";
+    durationLabel.frame = (CGRect){10,180,80,20};
+    [self.view addSubview:durationLabel];
+    
+    UITextField *durationField = [[UITextField alloc]init];
+    durationField.borderStyle = UITextBorderStyleRoundedRect;
+    durationField.frame = (CGRect){140, 180, 80, 20};
+    [self.view addSubview:durationField];
+    self.durationField = durationField;
+    
+    UILabel *repeatLabel = [[UILabel alloc]init];
+    repeatLabel.text = @"RepeatCount:";
+    repeatLabel.frame = (CGRect){10,220,120,20};
+    [self.view addSubview:repeatLabel];
+    
+    UITextField *repeatField = [[UITextField alloc]init];
+    repeatField.borderStyle = UITextBorderStyleRoundedRect;
+    repeatField.frame = (CGRect){140, 220, 80, 20};
+    [self.view addSubview:repeatField];
+    self.repeatCountField = repeatField;
+    
+}
+
+#pragma mark IBAction
+- (void)clickStartButtonEvent:(UIButton *)btn{
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    animation.keyPath = @"transform.rotation";
+    animation.byValue = @(M_PI * 2);
+    animation.duration = self.durationField.text.doubleValue;
+    animation.repeatCount = self.repeatCountField.text.floatValue;
+    animation.delegate = self;
+    [self.layer addAnimation:animation forKey:nil];
+    btn.enabled = NO;
+}
+#pragma mark CAAnimation Delegate
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag{
+    self.startButton.enabled = flag;
+}
+
+#pragma mark Touch Event
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+#pragma mark QR Code Image
 - (void)qrCodeImageDemo{
     CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
     [filter setDefaults];
@@ -56,7 +128,6 @@
 - (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size{
     CGRect extent = CGRectIntegral(image.extent);
     CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
-    
     // 1.创建bitmap;
     size_t width = CGRectGetWidth(extent) * scale;
     size_t height = CGRectGetHeight(extent) * scale;
