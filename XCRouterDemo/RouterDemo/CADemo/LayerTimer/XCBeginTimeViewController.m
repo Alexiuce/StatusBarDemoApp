cd8fb519c87150141cd84c4c53b2c19c228b24d0
alexiuce <alexiuce@qq.com>
2019-02-15 11:41:16 +0800

    refactor(#CADemo): XCBeginTimeViewController: 新增CAAnimationProcotol demo

---
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCBeginTimeViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCBeginTimeViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/2/15.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCBeginTimeViewController.h"
+
+@interface XCBeginTimeViewController ()
+
+@end
+
+@implementation XCBeginTimeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
