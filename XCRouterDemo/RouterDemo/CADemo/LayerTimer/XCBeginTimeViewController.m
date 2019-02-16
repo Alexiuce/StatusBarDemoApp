55a37816b9084a72d0d3d5887ca350f37608207d
alexiuce <alexiuce@qq.com>
2019-02-16 12:51:57 +0800

    refactor(#CADemo): #begin time , offset ,speed

--- XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCBeginTimeViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCBeginTimeViewController.m
@@ -19,14 +19,8 @@
     // Do any additional setup after loading the view from its nib.
 }
 
-/*
-#pragma mark - Navigation
+/** Core Animation Time 相对时间 : begin time , time off , speed , */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
