e4200b082b5c409c355c6063407edf315ba8db43
alexiuce <alexiuce@qq.com>
2019-02-22 08:38:10 +0800

    refactor: #CADemo:XCTimeCustomFuncViewController

---
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeCustomFuncViewController.m
@@ -0,0 +1,32 @@
+//
+//  XCTimeCustomFuncViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/2/22.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCTimeCustomFuncViewController.h"
+
+@interface XCTimeCustomFuncViewController ()
+
+@end
+
+@implementation XCTimeCustomFuncViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
