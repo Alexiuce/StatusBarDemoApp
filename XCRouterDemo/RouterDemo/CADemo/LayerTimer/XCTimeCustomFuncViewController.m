0e995aed4e8a36b41a837f57814890db58bb506f
alexiuce <alexiuce@qq.com>
2019-02-23 19:17:25 +0800

    refactor(#CADemo): XCTimeCustomFuncViewController: 显示CAMediFunction graph

--- XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeCustomFuncViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeCustomFuncViewController.m
@@ -17,16 +17,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self customEasingFuncDemo];
 }
 
-/*
-#pragma mark - Navigation
+- (void)customEasingFuncDemo{
+    CAMediaTimingFunction *fn = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    float ctrp1[2],ctrp2[2];
+    [fn getControlPointAtIndex:1 values:ctrp1];
+    [fn getControlPointAtIndex:2 values:ctrp2];
+    
+    
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointZero];
+    [path addCurveToPoint:(CGPoint){1,1} controlPoint1:(CGPoint){ctrp1[0],ctrp1[1]} controlPoint2:(CGPoint){ctrp2[0],ctrp2[1]}];
+    [path applyTransform:CGAffineTransformMakeScale(200, 200)];
+    
+    
+    CALayer *cl = [CALayer layer];
+    cl.frame = (CGRect){10,100,300,300};
+    [self.view.layer addSublayer:cl];
+    cl.geometryFlipped = YES;
+    
+    
+    CAShapeLayer *layer = [CAShapeLayer layer];
+    layer.path = path.CGPath;
+    layer.lineWidth = 4;
+    layer.strokeColor = UIColor.redColor.CGColor;
+    layer.fillColor = UIColor.clearColor.CGColor;
+    [cl addSublayer:layer];
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
 
 @end
