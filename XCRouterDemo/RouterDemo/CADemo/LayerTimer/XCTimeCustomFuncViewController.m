1a6937ef8af7b52723704e3007c0262509403cba
alexiuce <alexiuce@qq.com>
2019-03-01 09:15:15 +0800

    refactor(#CADemo):

--- XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeCustomFuncViewController.m
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeCustomFuncViewController.m
@@ -95,36 +95,8 @@
                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn],
                                  ];
     animation.keyTimes = @[@0.0,@0.3,@0.5,@0.7,@0.8,@0.9,@0.95,@(1.0)];
-//
-//
-//
-//    [self.ballView.layer addAnimation:animation forKey:nil];
     
     self.ballView.center = CGPointMake(150, 32);
-    
-//    //create keyframe animation
-//    CAKeyframeAnimation *animation = [CAKeyframeAnimation animation];
-//    animation.keyPath = @"position";
-//    animation.duration = 1.0;
-//    animation.values = @[[NSValue valueWithCGPoint:CGPointMake(150, 32)],
-//                        [NSValue valueWithCGPoint:CGPointMake(150, 268)],
-//                        [NSValue valueWithCGPoint:CGPointMake(150, 140)],
-//                         [NSValue valueWithCGPoint:CGPointMake(150, 268)],
-//                         [NSValue valueWithCGPoint:CGPointMake(150, 220)],
-//                         [NSValue valueWithCGPoint:CGPointMake(150, 268)],
-//                         [NSValue valueWithCGPoint:CGPointMake(150, 250)],
-//                         [NSValue valueWithCGPoint:CGPointMake(150, 268)]];
-//    animation.timingFunctions = @[
-//                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn],
-//                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut],
-//                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn],
-//                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut],
-//                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn],
-//                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut],
-//                                  [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn]
-//                                  ];
-//    animation.keyTimes = @[@0.0, @0.3, @0.5, @0.7, @0.8, @0.9, @0.95, @1.0];
-    
 
     //apply animation
     self.ballView.layer.position = CGPointMake(150, 268);
