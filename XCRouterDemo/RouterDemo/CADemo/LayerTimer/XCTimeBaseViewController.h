edf2ca080e8af70be164ac63708ed0dfb7100c1d
alexiuce <alexiuce@qq.com>
2019-03-15 08:11:14 +0800

    refactor(#CADdmeo): time base animation

---
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeBaseViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCTimeBaseViewController.h
+//  CADemo
+//
+//  Created by Alexcai on 2019/3/15.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTimeBaseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCRouterDemo/RouterDemo/CADemo/LayerTimer/XCTimeBaseViewController.m
@@ -0,0 +1,24 @@
+//
+//  XCTimeBaseViewController.m
+//  CADemo
+//
+//  Created by Alexcai on 2019/3/15.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCTimeBaseViewController.h"
+
+@interface XCTimeBaseViewController ()
+
+@end
+
+@implementation XCTimeBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+
+@end
