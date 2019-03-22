e1593515653e8d18ce678cb7181ab0394fe7b0fa
alexiuce <alexiuce@qq.com>
2019-03-22 17:29:19 +0800

    refactor(#NSView first Responder active):

---
+++ XCRouterDemo/RouterDemo/CoreAnimationForMac/XCView.h
@@ -0,0 +1,17 @@
+//
+//  XCView.h
+//  CoreAnimationForMac
+//
+//  Created by Alexcai on 2019/3/22.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCRouterDemo/RouterDemo/CoreAnimationForMac/XCView.m
@@ -0,0 +1,17 @@
+//
+//  XCView.m
+//  CoreAnimationForMac
+//
+//  Created by Alexcai on 2019/3/22.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+#import "XCView.h"
+
+@implementation XCView
+
+- (BOOL)acceptsFirstResponder{
+    return YES;
+}
+
+@end
