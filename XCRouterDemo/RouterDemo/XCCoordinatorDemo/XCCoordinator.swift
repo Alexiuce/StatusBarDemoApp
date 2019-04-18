990f3e0bf21cbb991ff9e8c55893a1ea623f6b5a
alexiuce <alexiuce@qq.com>
2019-04-18 15:01:50 +0800

    feat(#XCCoordiantorDemo): 添加协作器模式示例(Swift版本)

---
+++ XCRouterDemo/RouterDemo/XCCoordinatorDemo/XCCoordinator.swift
@@ -0,0 +1,16 @@
+//
+//  XCCoordinator.swift
+//  XCCoordinatorDemo
+//
+//  Created by Alexcai on 2019/4/18.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol XCCoordinator {
+    var chileCoordinators : [XCCoordinator] {get set}
+    var navigationController : UINavigationController {get set}
+    func start()
+}
---
+++ XCRouterDemo/RouterDemo/XCCoordinatorDemo/XCCreateAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  XCCreateAccountViewController.swift
+//  XCCoordinatorDemo
+//
+//  Created by Alexcai on 2019/4/18.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+import UIKit
+
+class XCCreateAccountViewController: UIViewController,XCStoryboard {
+
+    weak var coordinator: MainCoordinator?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
