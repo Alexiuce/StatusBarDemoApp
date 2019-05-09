988eae4360452a3bac3947a1b122025e8b66e3c8
alexiuce <alexiuce@qq.com>
2019-05-09 12:59:14 +0800

    feat(#App): 添加Adaptive Layout Controller

---
+++ XCRouterDemo/RouterDemo/AdaptiveRaywenderlich/XCAdaptiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  XCAdaptiveViewController.swift
+//  AdaptiveRaywenderlich
+//
+//  Created by Alexcai on 2019/5/9.
+//  Copyright © 2019 dongjiu. All rights reserved.
+//
+
+import UIKit
+
+class XCAdaptiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
